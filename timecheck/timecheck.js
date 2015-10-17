ada7de8c3fcf145e29f8466948a755f9b4a778f7
Jorne Roefs <jorneroefs@gmail.com>
2015-10-17 13:50:08 +0200

    Bugfix in time validation and License changed to MIT

--- timecheck/timecheck.js
+++ timecheck/timecheck.js
@@ -5,12 +5,20 @@ module.exports = function(RED) {
     var time = config.time.split(':'),
         checktime = new Date();
 
-    if (hours = parseInt(time[0]) && 0 < hours && hours < 24 && minutes = parseInt(time[1]) && 0 < minutes && minutes < 60) {
-      checktime.setHours(hours);
-      checktime.setMinutes(minutes);
+    if (time.length != 2) {
+      this.error("Time format must be HH:mm");
     }
 
-    
+    var hours = parseInt(time[0]),
+        minutes = parseInt(time[1]);
+
+    if (0 < hours && hours < 24 && 0 < minutes && minutes < 60) {
+      checktime.setHours(hours);
+      checktime.setMinutes(minutes);
+    } else {
+      this.error("Given time is not within valid range");
+    }
+
     this.checktime = checktime;
 
     var node = this;
